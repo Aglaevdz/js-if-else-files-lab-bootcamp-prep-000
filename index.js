05df34d062c3a65fb93b8f6ea9eecf2aa6c496e5
Aglae Verduzco <aglae.cv@gmail.com>
2018-09-08 19:02:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var height = "74"
 if (name === "Susan") {
   console.log("he name is correct")
 }
-
+else {
+  console.log("Except the name to be Susan")
+}
 // Don't worry about this
 module.exports = { name, height
 }
